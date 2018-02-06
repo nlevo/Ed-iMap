e3ce4d23501857f563ea2f3a1442e4910d0acaa4
nlevo <nikola.levo@gmail.com>
2018-02-05 19:13:59 -0500

    first commit

---
+++ js/map_world.js
@@ -0,0 +1,10 @@
+// var map = AmCharts.makeChart( "chartdiv2", {
+//     "type": "map",
+//     "theme": "light",
+//     "colorSteps": 10,
+  
+//     "dataProvider": {
+//       "map": "worldLow",
+//       "areas": []
+//     }
+//   } );
